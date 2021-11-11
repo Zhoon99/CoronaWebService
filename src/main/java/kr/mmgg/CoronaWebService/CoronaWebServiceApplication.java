45e29fed906336ffc1ea6c3d75a79eabc4aa0400
Zhoon99 <chjh827@gmail.com>
2021-11-11 20:56:28 +0900

    init

---
+++ src/main/java/kr/mmgg/CoronaWebService/CoronaWebServiceApplication.java
@@ -0,0 +1,13 @@
+package kr.mmgg.CoronaWebService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoronaWebServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoronaWebServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kr/mmgg/CoronaWebService/CoronaWebServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.mmgg.CoronaWebService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoronaWebServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
